486137ca692a3faa6eecc06909a89388796b27f8
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 10:00:59 -0600

    check box for item

--- src/components/Task/Item/index.tsx
+++ src/components/Task/Item/index.tsx
@@ -8,7 +8,24 @@ function Item(props: ItemProps) {
 
   useEffect(() => setToDo(props.todo), [props.todo]);
 
-  return <div className={style.item}>{toDo.title}</div>;
+  return (
+    <div className={style.item}>
+      <div className={style["check-wrapper"]}>
+        <input
+          className={style.check}
+          type="checkbox"
+          checked={toDo.completed}
+          onClick={() => setToDo({ ...toDo, completed: !toDo.completed })}
+        />
+        <span
+          style={{ textDecoration: toDo.completed ? "line-through" : "none" }}
+        >
+          {toDo.title}
+        </span>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Item;
