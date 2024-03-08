dcaa92c49024549cb832dc01a3bfaf767586b52c
Nima Sherpa <sherpa2025@gmail.com>
2024-03-08 09:37:39 -0700

    home page changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,46 @@
 import { getRoute } from "../../routes";
-import Link from "../../components/Link";
+import Title from "../../components/Title";
+import { Link } from "react-router-dom";
+
+import style from "./home.module.css";
+import Input from "../../components/Input";
+import List from "../../components/Task/List";
+import { useState } from "react";
+import Button from "../../components/Button";
 
 function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const toDoSample = [
+    {
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "quis ut nam facilis et officia qui",
+      completed: false,
+    },
+  ];
+
   return (
-    <div>
-      Home
-      <br />
-      <br />
-      <Link to={getRoute("components")}>Go to components</Link>
+    <div className={style.page}>
+      <Title>Todo App</Title>
+      <div className={style.container}>
+        <Input
+          className={style.input}
+          placeholder="Add Task"
+          value={inputValue}
+          onChange={setInputValue}
+        />
+        <Button className={style.button} variant="success">
+          Success
+        </Button>
+      </div>
+
+      <List toDoList={toDoSample} />
     </div>
   );
 }
