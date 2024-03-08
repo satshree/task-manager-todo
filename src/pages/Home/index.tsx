c1004e0d58091a7d573ea524f121df82da6e4848
Nima Sherpa <sherpa2025@gmail.com>
2024-03-08 10:30:07 -0700

    create add button logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,16 +5,17 @@ import Input from "../../components/Input";
 import List from "../../components/Task/List";
 import Button from "../../components/Button";
 import getTasks from "../../utils/index";
+import { ToDoData } from "../../types";
 
 function Home() {
   const [inputValue, setInputValue] = useState("");
-  const [toDoList, setToDoList] = useState([]);
+  const [toDoList, setToDoList] = useState<ToDoData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getTasks();
-        const firstTenTasks = data.slice(0, 10); // Get the first 10 tasks
+        const firstTenTasks = data.slice(0, 7);
         setToDoList(firstTenTasks);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -24,7 +25,23 @@ function Home() {
     fetchData();
   }, []);
 
-  const handleAddTask = () => {};
+  const handleAddTask = () => {
+    if (inputValue.trim() !== "") {
+      // random id between 1-100
+      const userId = Math.floor(Math.random() * 100) + 1;
+      // id based on the lenght of the todoList array + 1
+      const id = toDoList.length + 1;
+      // newTodo list
+      const newTodo: ToDoData = {
+        userId: userId,
+        id: id,
+        title: inputValue.trim(),
+        completed: false, // task set always false in initial
+      };
+      setToDoList([...toDoList, newTodo]);
+      setInputValue("");
+    }
+  };
 
   return (
     <div className={style.page}>
@@ -45,7 +62,11 @@ function Home() {
         </div>
       </div>
 
-      <List toDoList={toDoList} />
+      <List
+        toDoList={toDoList}
+        edit={(id: number) => alert(`EDIT TODO ID ${id}`)}
+        delete={(id: number) => alert(`DELETE TODO ID ${id}`)}
+      />
     </div>
   );
 }
