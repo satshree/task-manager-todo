7c7bae5b36ae8752c814a4de81a2418545c2ba59
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 16:25:38 -0600

    throw errors from api functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,23 +4,35 @@ const API_ROOT = "https://jsonplaceholder.typicode.com/todos";
 
 export async function getTask(id: number) {
   const result = await fetch(API_ROOT + `/${id}/`);
-  const taskJSON: ToDoData = await result.json();
 
+  if (result.status !== 200) {
+    throw new Error(result.statusText);
+  }
+
+  const taskJSON: ToDoData = await result.json();
   return taskJSON;
 }
 
 export async function getTasks() {
   const result = await fetch(API_ROOT);
-  const tasksJSON: ToDoData[] = await result.json();
 
+  if (result.status !== 200) {
+    throw new Error(result.statusText);
+  }
+
+  const tasksJSON: ToDoData[] = await result.json();
   return tasksJSON;
 }
 
 export async function postTask(task: ToDoData) {
-  await fetch(API_ROOT, {
+  const result = await fetch(API_ROOT, {
     method: "POST",
     body: JSON.stringify(task),
   });
+
+  if (result.status !== 200) {
+    throw new Error(result.statusText);
+  }
 
   return task;
 }
