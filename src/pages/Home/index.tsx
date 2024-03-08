14c8a489eb7ed03aa12115c3c731a188a302dd81
Nima Sherpa <sherpa2025@gmail.com>
2024-03-08 10:16:49 -0700

    data fetch from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,46 +1,51 @@
-import { getRoute } from "../../routes";
+import React, { useState, useEffect } from "react";
 import Title from "../../components/Title";
-import { Link } from "react-router-dom";
-
 import style from "./home.module.css";
 import Input from "../../components/Input";
 import List from "../../components/Task/List";
-import { useState } from "react";
 import Button from "../../components/Button";
+import getTasks from "../../utils/index";
 
 function Home() {
   const [inputValue, setInputValue] = useState("");
-  const toDoSample = [
-    {
-      userId: 1,
-      id: 1,
-      title: "delectus aut autem",
-      completed: false,
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "quis ut nam facilis et officia qui",
-      completed: false,
-    },
-  ];
+  const [toDoList, setToDoList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTasks();
+        const firstTenTasks = data.slice(0, 10); // Get the first 10 tasks
+        setToDoList(firstTenTasks);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleAddTask = () => {};
 
   return (
     <div className={style.page}>
       <Title>Todo App</Title>
       <div className={style.container}>
-        <Input
-          className={style.input}
-          placeholder="Add Task"
-          value={inputValue}
-          onChange={setInputValue}
-        />
-        <Button className={style.button} variant="success">
-          Success
-        </Button>
+        <div>
+          <Input
+            className={style.input}
+            placeholder="Add Task"
+            value={inputValue}
+            onChange={setInputValue}
+          />
+        </div>
+        <div className={style.button}>
+          <Button variant="success" onClick={handleAddTask}>
+            Add
+          </Button>
+        </div>
       </div>
 
-      <List toDoList={toDoSample} />
+      <List toDoList={toDoList} />
     </div>
   );
 }
