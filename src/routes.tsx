72d5f4c4c251fa8dab6cbaeeb42270b6bc666dac
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 16:44:36 -0600

    add wildcard page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Routes } from "./types";
 import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import ComponentList from "./pages/ComponentList";
+import NotFound from "./pages/NotFound";
 
 export const routes: Routes[] = [
   {
@@ -19,6 +20,11 @@ export const routes: Routes[] = [
     to: "/components",
     text: "components",
     element: <ComponentList />,
+  },
+  {
+    to: "/*",
+    text: "wildcard",
+    element: <NotFound />,
   },
 ];
 
