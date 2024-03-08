fed53bfb0554348b3cd881828e62cc9ddf4f7706
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 10:26:50 -0600

    post task api function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,9 +2,18 @@ import { ToDoData } from "../types";
 
 const API_ROOT = "https://jsonplaceholder.typicode.com/todos";
 
-export default async function getTasks() {
+export async function getTasks() {
   const result = await fetch(API_ROOT);
   const tasksJSON: ToDoData[] = await result.json();
 
   return tasksJSON;
+}
+
+export async function postTask(task: ToDoData) {
+  await fetch(API_ROOT, {
+    method: "POST",
+    body: JSON.stringify(task),
+  });
+
+  return task;
 }
