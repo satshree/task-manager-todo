646144aa0c18833f7656eb5960be13d2bfdba735
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 16:41:28 -0600

    display error message on edit page

--- src/pages/Edit/index.tsx
+++ src/pages/Edit/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import style from "./edit.module.css";
@@ -9,6 +9,7 @@ import Box from "../../components/Box";
 import { getRoute } from "../../routes";
 import { getState, saveState } from "../../utils/LocalStorage";
 import { getTaskFromList } from "../Home";
+import LinkButton from "../../components/Button/LinkButton";
 
 function Edit() {
   const { id } = useParams<string>();
@@ -17,9 +18,16 @@ function Edit() {
   // STATES
   let allTasks = getState();
   const task = getTaskFromList(parseInt(String(id)), allTasks);
-  const [title, setTitle] = useState(task.title);
+  const [error, setError] = useState("");
+  const [title, setTitle] = useState("");
 
-  // useEffect(() => setTitle(task.title), []);
+  useEffect(() => {
+    if (task.id === 0) {
+      setError("Task Does Not Exist");
+    } else {
+      setTitle(task.title);
+    }
+  }, []);
 
   const handleEdit = (e: FormEvent) => {
     e.preventDefault();
@@ -36,24 +44,35 @@ function Edit() {
 
   return (
     <div className={style["edit-page"]}>
-      <Box border={true}>
-        <form onSubmit={handleEdit} style={{ width: "100%" }}>
-          <Input
-            label="Task Title"
-            placeholder="Productive Task"
-            value={title}
-            onChange={(title) => setTitle(title)}
-            required={true}
-          />
-          <br />
-          <Button type="submit" variant="success">
-            Save Changes
-          </Button>
-          <Button type="button" onClick={handleNavigation}>
-            Go Back
-          </Button>
-        </form>
-      </Box>
+      {error ? (
+        <>
+          <Box center={true}>{error}</Box>
+          <Box center={true}>
+            <LinkButton to={getRoute("home")}>Go Back</LinkButton>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box border={true}>
+            <form onSubmit={handleEdit} style={{ width: "100%" }}>
+              <Input
+                label="Task Title"
+                placeholder="Productive Task"
+                value={title}
+                onChange={(title) => setTitle(title)}
+                required={true}
+              />
+              <br />
+              <Button type="submit" variant="success">
+                Save Changes
+              </Button>
+              <Button type="button" onClick={handleNavigation}>
+                Go Back
+              </Button>
+            </form>
+          </Box>
+        </>
+      )}
     </div>
   );
 }
