50c1e9347c3e8d7694d8eaf86afbaec7992cad9b
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-03-08 13:15:51 -0600

    fetch task detail api

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,13 @@ import { ToDoData } from "../types";
 
 const API_ROOT = "https://jsonplaceholder.typicode.com/todos";
 
+export async function getTask(id: number) {
+  const result = await fetch(API_ROOT + `/${id}/`);
+  const taskJSON: ToDoData = await result.json();
+
+  return taskJSON;
+}
+
 export async function getTasks() {
   const result = await fetch(API_ROOT);
   const tasksJSON: ToDoData[] = await result.json();
